15443f5fadd0e89f47566d0ee3a6059519fcd6f6
kaljuk <erik404@gmail.com>
2017-12-21 17:53:19 +0200

    How to document a function

--- lol_backend.py
+++ lol_backend.py
@@ -7,12 +7,15 @@ def printTest():
     print("Test successful")
 
 # Siin saame info kasutaja kohta
-# [r- UIselectedRegion, summonerName]
+# [r- UIselectedRegion, n- summonerName]
 def getData(r, n, api_key):
     region = "eun1" if (r=="EUNE") else "euw1" if (r=="EUW") else "na1"
     url = "https://<REGION>.api.riotgames.com/lol/summoner/v3/summoners/by-name/<NAME>?api_key="+api_key
-    url.replace('<REGION>', region)
-    url.replace('<NAME>', n)
+    url = url.replace('<REGION>', region).replace('<NAME>', n)
     json_data = urlopen(url)
     data = json.load(json_data)
     return data
+# FUNCT <- (selectedRegion, summonerName); example ["EUNE", "Kajakaboss"]
+# WAIT
+# FUNC -> dict {id: summonerId, accountId: accountId, etc};
+# example      {id: 123123123, accountId: 121231231, etc}
