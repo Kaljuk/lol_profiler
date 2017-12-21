0cdb34f36e1b484d7293aeb3a509ebe73013df69
kaljuk <erik404@gmail.com>
2017-12-21 16:49:00 +0200

    Importing lol_backend and tkinter

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # Erik Kaljumäe ja Gregor Nõges
 # LoL Profiler 2017
 
-
+##  NonLocal Imports
+import tkinter as tk
+## Local Imports
 # Import backend
 import lol_backend as be
 
+
 # This is the main file
 be.printTest()
+
+
+# A new window
