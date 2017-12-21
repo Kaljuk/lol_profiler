7ebaa02ab49194850f8b41a687055f732a4a2235
kaljuk <erik404@gmail.com>
2017-12-21 23:33:01 +0200

    Added raw spec function to backend for refactoring

--- lol_backend.py
+++ lol_backend.py
@@ -6,8 +6,8 @@ import json
 def printTest():
     print("Test successful")
 
-# Siin saame info kasutaja kohta
-# [r- UIselectedRegion, n- summonerName]
+# Get user data from riot api
+# getData[r- UIselectedRegion, n- summonerName]
 def getData(r, n, api_key):
     region = "eun1" if (r=="EUNE") else "euw1" if (r=="EUW") else "na1"
     url = "https://<REGION>.api.riotgames.com/lol/summoner/v3/summoners/by-name/<NAME>?api_key="+api_key
@@ -19,3 +19,10 @@ def getData(r, n, api_key):
 # WAIT
 # FUNC -> dict {id: summonerId, accountId: accountId, etc};
 # example      {id: 123123123, accountId: 121231231, etc}
+
+
+def spec(id): # Siin saame infot käimasoleva mängu kohta (overall) [kui mängu pole, siis tuleb 404 error!]
+    url = "https://na1.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/"+str(id)+"?api_key="+api_key
+    json_data = urlopen(url)
+    andmed = json.load(json_data)
+    return andmed
