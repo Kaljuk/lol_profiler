c96490fe3e631548717efee141c079ec21d4c5da
kaljuk <erik404@gmail.com>
2017-12-21 17:42:03 +0200

    Refactoring getData function to lol_backEnd

--- lol_backend.py
+++ lol_backend.py
@@ -1,3 +1,18 @@
+# Import stuff to backend
+from urllib.request import urlopen
+import json
+
 # Here goes all the backend functions
 def printTest():
     print("Test successful")
+
+# Siin saame info kasutaja kohta
+# [r- UIselectedRegion, summonerName]
+def getData(r, n, api_key):
+    region = "eun1" if (r=="EUNE") else "euw1" if (r=="EUW") else "na1"
+    url = "https://<REGION>.api.riotgames.com/lol/summoner/v3/summoners/by-name/<NAME>?api_key="+api_key
+    url.replace('<REGION>', region)
+    url.replace('<NAME>', n)
+    json_data = urlopen(url)
+    data = json.load(json_data)
+    return data
